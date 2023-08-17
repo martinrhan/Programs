9d194af45d26f7754d22208666de63dac1eb44a0
Martin Han <martinrhan@126.com>
2023-08-18 07:40:22 +1000

    Add project files.

---
+++ CloudSeedingData/Views/MainWindow.axaml.cs
@@ -0,0 +1,50 @@
+﻿using Avalonia.Controls;
+using ScottPlot;
+using ScottPlot.Avalonia;
+using ScottPlot.Plottable;
+using ScottPlot.Statistics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CloudSeedingData.Views;
+
+public partial class MainWindow : Window {
+    static string Directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)!;
+    public MainWindow() {
+        InitializeComponent();
+
+        AvaPlot1.Plot.XAxis.Grid(false);
+        AvaPlot1.Plot.XTicks(new string[] { "Control" , "Seeded" });
+
+        string[] lines = ReadLines("CloudSeedingData.csv").Skip(1).ToArray();
+
+        double[] control = new double[lines.Length];
+        double[] seeded = new double[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++) {
+            string line = lines[i];
+            string[] numbers = line.Split(',');
+            control[i] = double.Parse(numbers[0]);
+            seeded[i] = double.Parse(numbers[1]);
+        }
+
+        Population population_control = new Population(control);
+        Population population_seeded = new Population(seeded);
+        PopulationPlot populationPlot = AvaPlot1.Plot.AddPopulations(new Population[] { population_control , population_seeded});
+        populationPlot.DistributionCurve = false;
+        populationPlot.DataFormat = PopulationPlot.DisplayItems.ScatterOnBox;
+        populationPlot.DataBoxStyle = PopulationPlot.BoxStyle.BoxMedianQuartileOutlier;
+        AvaPlot1.Refresh();
+    }
+
+    internal static IEnumerable<string> ReadLines(string fileName) {
+        StreamReader reader;
+        string? line;
+        reader = new(Directory + @"\" + fileName);
+        reader.ReadLine();
+        while ((line = reader.ReadLine()) is not null) {
+            yield return line;
+        }
+    }
+}
---
+++ ThrowBallProbabilityTest/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Random random = new Random();
+
+bool test() {
+    int box1 = 0, box2 = 0, box3 = 0;
+    for (int i = 0; i < 10; i++) {
+        int int_random = random.Next(0, 4);
+        switch (int_random) {
+            case 0:
+                box1++;
+                break;
+            case 1 or 2:
+                box2++;
+                break;
+            case 3:
+                box3++;
+                break;
+        }
+    }
+    return (box1 == 2 && box2 == 5 && box3 == 3);
+}
+
+int passed = 0;
+for  (int i = 0; i < 100000; i++) {
+    if (test()) passed++;
+}
+
+Console.WriteLine(passed / 100000d);
