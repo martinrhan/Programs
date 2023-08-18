90c5806b4371b4b95134531f322bb96a0e13289e
Martin Han <martinrhan@126.com>
2023-08-18 14:21:42 +1000

    add projects for the continuous random variable question

---
+++ ContinuousRandomVariable/Views/MainWindow.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Controls;
+using System;
+
+namespace ContinuousRandomVariable.Views;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+
+        static double Square(double x) {
+            return x * x;
+        }
+        static double F(double x) {
+            return Math.Pow(Math.E, -x) / Square(1 + Math.Pow(Math.E, -x));
+        }
+
+        double[] array_y = new double[10000];
+        double max_x = 100;
+        for (int i = 0; i < array_y.Length; i++) {
+            double x = (double)i / (double)array_y.Length * max_x;
+            double y = F(x);
+            array_y[i] = y;
+        }
+
+        AvaPlot1.Plot.AddSignal(array_y, max_x);
+
+        AvaPlot1.Refresh();
+    }
+}
